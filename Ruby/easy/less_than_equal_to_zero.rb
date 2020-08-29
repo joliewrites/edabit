99e918c66fb7777fae10b3c4488bee30d396e186
Jolie Adam <as.jolie3@gmail.com>
2020-08-28 21:52:43 -0400

    ruby

---
+++ Ruby/easy/less_than_equal_to_zero.rb
@@ -0,0 +1,7 @@
+def less_than_or_equal_to_zero(num)
+  if num <= 0
+    return true
+  else
+    return false
+  end
+end
---
+++ Ruby/easy/same_num.rb
@@ -0,0 +1,7 @@
+def is_same_num(num1, num2)
+  if num1 == num2
+    return true
+  else
+    return false
+  end
+end
