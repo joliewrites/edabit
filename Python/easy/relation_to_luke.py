6f0e7663804bdc42e63af3f0bee2e1d1b8768a91
Jolie Adam <as.jolie3@gmail.com>
2020-09-01 20:03:08 -0400

    py

---
+++ Python/easy/relation_to_luke.py
@@ -0,0 +1,6 @@
+def relation_to_luke(name):
+  solutions = {'Leia':'sister',
+  'Darth Vader':'father',
+  'Han':'brother in law',
+  'R2D2':'droid'}
+  return 'Luke, I am your {}.'.format(solutions[name])
